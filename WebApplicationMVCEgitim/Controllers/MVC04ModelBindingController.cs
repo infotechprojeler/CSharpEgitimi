a6bd88c16500998c044f8c0adda8571e7a90bd4b
InfotechProjeler <ilica-18@hotmail.com>
2024-01-07 14:31:46 +0300

    MVC Model Kullanımı

---
+++ WebApplicationMVCEgitim/Controllers/MVC04ModelBindingController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVCEgitim.Models;
+
+namespace WebApplicationMVCEgitim.Controllers
+{
+    public class MVC04ModelBindingController : Controller
+    {
+        // GET: MVC04ModelBinding
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult KullaniciDetay() // Bir action metodunun varsayılan çalışma yöntemi httpget dir, bu yüzden metodun üzerine bu attribute ü eklemeye gerek yoktur.
+        {
+            Kullanici kullanici = new Kullanici()
+            {
+                Ad = "Murat",
+                Soyad = "Yılmaz",
+                Email = "muro@vadi.co",
+                KullaniciAdi = "murat",
+                Sifre = "1234"
+            }; // kullanıcıyı oluşturduk
+
+            ViewBag.KullaniciAdSoyad = kullanici.Ad + " " + kullanici.Soyad;
+
+            return View(kullanici); // burada view içerisine yukardaki kullanıcıyı göndermezsek ekranda bu bilgilere ulaşamayız!
+        }
+        [HttpPost]
+        public ActionResult KullaniciDetay(Kullanici kullanici) // bu şekilde view ekranında kullanılan model classını yakalayıp içindeki verilere ulaşabiliyoruz, buna mvc de model binding deniliyor.
+        {
+            // burada artık yakaladığımız kullanici nesnesini veritabanına kaydedebilir, güncelleyebilir veya silebiliriz.
+            return View(kullanici); // gelen yeni kullanıcı bilgisini tekrardan view ekranına yolluyoruz.
+        }
+        public ActionResult AdresDetay()
+        {
+            Adres adres = new Adres();
+            adres.Sehir = "İstanbul";
+            adres.Ilce = "Şişli";
+            adres.AcikAdres = "Mecidiyeköyü";
+            return View(adres);
+        }
+        [HttpPost]
+        public ActionResult AdresDetay(Adres adres)
+        {            
+            return View(adres);
+        }
+        public ActionResult KullaniciAdresDetay()
+        {
+            var model = new KullaniciAdresDetayViewModel();
+
+            Kullanici kullanici = new Kullanici()
+            {
+                Ad = "Murat",
+                Soyad = "Yılmaz",
+                Email = "muro@vadi.co",
+                KullaniciAdi = "murat",
+                Sifre = "1234"
+
+            };
+            Adres adres = new Adres();
+            adres.Sehir = "İstanbul";
+            adres.Ilce = "Şişli";
+            adres.AcikAdres = "Mecidiyeköyü";
+
+            model.Adres = adres;
+            model.Kullanici = kullanici;
+
+            return View(model);
+        }
+    }
+}
